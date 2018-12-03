a04f08b4d89e631e632cc268473bbbd7c3532ea2
Jonathan Rocha <jonathan.rocha@msn.com>
2018-12-03 15:41:51 -0300

    Changes the professor demanded

--- lenormand/js/app.out/boot/cljs/main821.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.9.562 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main821');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
-goog.require('online_lenormand.app');
-online_lenormand.app.init();
